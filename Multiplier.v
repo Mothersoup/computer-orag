1096e3d46ca794af47c52cc3753fbc8880701189
Borlon <imagirlfukk@gmail.com>
2024-05-21 22:42:42 +0800

    multiplier

--- Multiplier.v
+++ Multiplier.v
@@ -1,12 +1,15 @@
 `timescale 1ns/1ps
-module UnsignedMultiplier(  clk, dataA, dataB, reset, dataOut );
+module UnsignedMultiplier(  clk, dataA, dataB, reset, dataOut, signal );
 input clk, reset;
 input[31:0] dataA, dataB;
+input[5:0] signal;
 output reg [63:0] dataOut;
 reg [63:0] product;
-reg [5:0] counter;
+reg [4:0] counter;
 reg [31:0] dataA_wire;
 reg [31:0] temp;
+reg [5:0] prev_signal;
+parameter MUL = 6'b011001;
 
 always @( posedge clk or posedge reset ) begin
         if ( reset )
@@ -14,36 +17,33 @@ always @( posedge clk or posedge reset ) begin
             product <= 64'h0; // 將乘積 product 初始化為 0
             product <= 64'b0;
             counter <= 5'b0;
+            prev_signal <= 6'b0;
             dataA_wire <= dataA;
-            dataOut <= 64'b0;
-
             end 
             
         else
             begin
-            
-                if ( counter < 6'b100000 )
-                    begin
-                    if ( dataA_wire[0] == 1'b1 )
-                        begin
+            if (signal == MUL && prev_signal != MUL ) begin
+                counter <= 6'b0; // 重置 counter
+                product <= 64'b0;
+                dataA_wire <= dataA;
+            end 
+            else begin
+                if ( counter < 6'b100000 )begin
+                    if ( dataA_wire[0] == 1'b1 )begin
                             temp = product[63:32] + dataB;
-                            product = {temp, product[31:0] };
-                                                                                    
-
+                            product = {product[63:32], temp };
                         end // if 
-                        
-                    dataA_wire <= {1'b0, dataA_wire[31:1]};
-                    product <= {1'b0, product[63:1]};
+                    dataA_wire <= dataA_wire >> 1;
+                    product <= product >> 1;
                     counter <= counter + 1;
-            
-                                                                                           
-                    end
-                else
-                    begin
+  
 
-                        dataOut <= product;
-                                                                                          
                     end
+                else begin
+                        dataOut <= product;
+                    end
+            end
                     
             end
     
--- alu/alu/HiLo.v
+++ alu/alu/HiLo.v
@@ -1,5 +1,5 @@
 `timescale 1ns/1ns
-module HiLo( clk, DivAns, HiOut, LoOut, reset );
+module HiLo( clk, mutiAns, HiOut, LoOut, reset );
 input clk ;
 input reset ;
 input [63:0] DivAns ;
@@ -8,10 +8,7 @@ output [31:0] LoOut ;
 
 reg [63:0] HiLo ;
 
-/*
-=====================================================
-下面為模擬範例，程式撰寫請遵照老師上課說明的方法來寫
-=====================================================
+
 */
 always@( posedge clk or reset )
 begin
@@ -24,7 +21,7 @@ reset訊號 如果是reset就做歸0
 */
   else
   begin
-    HiLo = DivAns ;
+    HiLo = mutiAns ;
   end
 /*
 把傳入的除法答案存起來
@@ -33,9 +30,5 @@ end
 
 assign HiOut = HiLo[63:32] ;
 assign LoOut = HiLo[31:0] ;
-/*
-=====================================================
-上面為模擬範例，程式撰寫請遵照老師上課說明的方法來寫
-=====================================================
-*/
+
 endmodule
